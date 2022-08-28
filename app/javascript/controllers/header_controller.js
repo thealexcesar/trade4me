3a54a815ad9d78b45d7673b766720358f6bc894e
thealexcesar <thealexcesar@gmail.com>
2022-08-28 00:11:23 -0300

    stimulus el-transitions

--- app/javascript/controllers/header_controller.js
+++ app/javascript/controllers/header_controller.js
@@ -1,7 +1,13 @@
 import { Controller } from "@hotwired/stimulus"
+import {enter, leave, toggle} from 'el-transition'
 
 export default class extends Controller {
+  static targets = ['openUserMenu']
   connect() {
-    console.log("Hello World!");
+    this.openUserMenuTarget.addEventListener('click', this.toggleDropdownMenu)
+  }
+
+  toggleDropdownMenu() {
+    toggle(document.getElementById('menu-dropdown-items'));
   }
 }
--- notes.md
+++ notes.md
@@ -8,3 +8,7 @@ rails assets:precompile # is compiling and loading the CSS just fine.
 bin/rails tailwindcss:watch # is succeeding and saying
 # But, if I clean the built files - then Rails isn't loading the file from the build/ folder.
 ```
+### importmaps
+```bash
+./bin/importmap pin  # example ➱ el-transition 
+```
