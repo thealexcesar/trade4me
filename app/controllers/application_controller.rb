ff1abcbb69ef8c1d76a2048dc56888e1c75982bc
thealexcesar <thealexcesar@gmail.com>
2022-08-28 16:03:07 -0300

    UX UI login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+
+  protected
+  def admin?
+    if self.level == :admin
+      true
+    else
+      redirect_to, flash[:alert] = I18n.t('general.error_message.denied') # Fixme!
+      return false
+    end
+  end
 end
